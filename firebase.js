f58b47373a365e846c38fa68dfa772fe9b9400dd
ProfesorRafaelBiano87 <biano.rafael@escola.pr.gov.br>
2026-06-19 11:51:33 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,16 +1,17 @@
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
-  projectId: "SEU_PROJETO",
-  storageBucket: "SEU_PROJETO.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+  apiKey: "AIzaSyDGX1BUKMGbbRLzbBU9yPLwtV_EHJyOV-M",
+  authDomain: "bolao-2-dbcc8.firebaseapp.com",
+  projectId: "bolao-2-dbcc8",
+  storageBucket: "bolao-2-dbcc8.firebasestorage.app",
+  messagingSenderId: "797320892122",
+  appId: "1:797320892122:web:a5f55c616167796b30dfe3"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-window.db = db;
